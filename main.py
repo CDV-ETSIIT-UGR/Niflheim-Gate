108e22405cc94da2d8c3a11cce390abb423138af
Alejandro Gomez <alejandrogomezmartin90@gmail.com>
2017-11-14 13:53:25 +0100

    Basic DOM configuration
    Basic DOM configuration and creation of basic files to start all.

---
+++ main.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import sys
+import os
+import json
+import urllib
+import time
+
+#from clases.MCRcon import MCRcon
+
+if __name__ == '__main__':
+    """ Manage main menu. """
+    #mcrcon1 = MCRcon()
+
+    """ load configuration """
+    if os.path.isfile(os.path.join('config.json')):
+        try:
+            json_data = open(os.path.join('config.json'))
+            game_config = json.load(json_data)
+            json_data.close()
+        except:
+            print("The file 'config.json' can not be loaded.")
+            exit(2)
+    else:
+        print("The file 'config.json' can not be find.")
+        exit(1)
+
+    """ load languges """
+    if os.path.isfile(os.path.join(game_config['default_language_file'])):
+        try:
+            json_data = open(os.path.join(game_config['default_language_file']))
+            game_config['lang'] = json.load(json_data)
+            json_data.close()
+            print(game_config['lang']['string_01'])
+        except:
+            print("The file '"+game_config['default_language_file']+"' can not be loaded.")
+            exit(3)
+    else:
+        print("The file '"+game_config['default_language_file']+"' can not be find.")
+        exit(4)
